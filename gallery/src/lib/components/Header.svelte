31aba9d8ba8cc95971b8aacbf2d4b8e5294d7cf0
Andrew Tork Baker <andrew.tork.baker@gmail.com>
2025-10-22 11:28:20 -0700

    Make header a link to the home page

--- gallery/src/lib/components/Header.svelte
+++ gallery/src/lib/components/Header.svelte
@@ -1,5 +1,7 @@
 <header class="bg-kidpix-orange border-b-4 border-black p-6 shadow-chunky-lg">
-	<h1 class="text-5xl font-black text-white uppercase text-center tracking-wide">
-		Claude Draws
+	<h1 class="text-5xl font-black text-black uppercase text-center tracking-wide">
+		<a href="/" class="text-black no-underline hover:opacity-80 transition-opacity bg-transparent">
+			Claude Draws
+		</a>
 	</h1>
 </header>
