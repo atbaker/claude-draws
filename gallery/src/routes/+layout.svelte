49706d002a522395554ff41dc63e4e6d0717dbc4
Andrew Tork Baker <andrew.tork.baker@gmail.com>
2025-10-21 23:33:03 -0700

    Gallery improvements, add favicon

--- gallery/src/routes/+layout.svelte
+++ gallery/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.ico" sizes="any" />
+	<link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+	<link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 
 {@render children?.()}
